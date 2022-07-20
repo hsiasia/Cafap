c6933b1a78c177cbbee92c4f71eb7cc69f6f41da
hsiasia <pompompurin221187@gmail.com>
2022-07-20 16:15:29 +0800

    add axios config to let frontend call laravel API

--- resources/assets/js/config.js
+++ resources/assets/js/config.js
@@ -4,3 +4,21 @@
  *-------------------------------------------------------------------------------
  * contain all config, all API URL base on enviroment...  
  */
+
+/**
+ * Defines the API route we are using.
+ */
+var api_url = '';
+
+switch( process.env.NODE_ENV ){
+    case 'development':
+        api_url = 'http://localhost/api/v1';
+        break;
+    case 'production':
+        api_url = 'http://localhost/api/v1';
+        break;
+}
+
+export const ROAST_CONFIG = {
+    API_URL: api_url,
+}
